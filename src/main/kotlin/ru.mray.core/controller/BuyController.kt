1e48302abd3919e0e5410df80eca85c4022d6695
Dmitry Romanov <me@wutiarn.ru>
2017-03-02 23:44:32 +0300

    Form processing method stub

--- src/main/kotlin/ru.mray.core/controller/BuyController.kt
+++ src/main/kotlin/ru.mray.core/controller/BuyController.kt
@@ -1,13 +1,24 @@
 package ru.mray.core.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 
 @Controller
 @RequestMapping("/buy")
 class BuyController {
+
+    val logger = LoggerFactory.getLogger(BuyController::class.java)
+
     @RequestMapping
-    fun buy(): String {
+    fun getPage(): String {
         return "buy"
+    }
+
+    @RequestMapping(method = arrayOf(RequestMethod.POST))
+    fun processForm() {
+        logger.info("Processing form...")
+//        TODO: Get user input and print it to log
     }
 }
