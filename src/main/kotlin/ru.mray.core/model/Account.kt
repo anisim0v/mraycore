be5fa0f8bd334be6829cbd8f329c796eb285c7db
Dmitry Romanov <me@wutiarn.ru>
2017-03-05 16:26:16 +0300

    WIP #4 Database models and new users saving
    Rollback to ordinary classes for models, fix mongo deserialization

--- src/main/kotlin/ru.mray.core/model/Account.kt
+++ src/main/kotlin/ru.mray.core/model/Account.kt
@@ -3,14 +3,20 @@ package ru.mray.core.model
 import java.time.Instant
 import java.util.*
 
-data class Account (
-        var email: String,
-        var region: Region,
-        var renewPeriod: Int = 1,
-        var provisioned: Boolean = false,
-        var activeUntil: Instant? = null,
-        val id: String = UUID.randomUUID().toString()
-)
+class Account() {
+    lateinit var email: String
+    lateinit var region: Region
+    var renewPeriod: Int = 1
+    var provisioned: Boolean = false
+    var activeUntil: Instant? = null
+    val id: String = UUID.randomUUID().toString()
+
+    constructor(email: String, region: Region, renewPeriod: Int) : this() {
+        this.email = email
+        this.region = region
+        this.renewPeriod = renewPeriod
+    }
+}
 
 enum class Region {
     PH,
--- src/main/kotlin/ru.mray.core/model/Transaction.kt
+++ src/main/kotlin/ru.mray.core/model/Transaction.kt
@@ -4,12 +4,16 @@ import java.time.Instant
 import java.time.Period
 import java.util.*
 
-data class Transaction(
-    var period: Period,
-    var issueDate: Instant = Instant.now(),
-    var previousPaymentId: String? = null,
-    var paidAt: Instant? = null,
-    var activatedAt: Instant? = null,
-    var activeUntil: Instant? = null,
+class Transaction() {
+    lateinit var period: Period
+    var issueDate: Instant = Instant.now()
+    var previousPaymentId: String? = null
+    var paidAt: Instant? = null
+    var activatedAt: Instant? = null
+    var activeUntil: Instant? = null
     val id: String = UUID.randomUUID().toString()
-)
+
+    constructor(period: Period) : this() {
+        this.period = period
+    }
+}
