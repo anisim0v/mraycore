0f70dfdd89f7b9df0762ddb0e1e17c901cb326c9
Dmitry Romanov <me@wutiarn.ru>
2017-03-06 20:22:46 +0300

    WIP #11 W1 confirmation parsing

--- src/main/kotlin/ru.mray.core/controller/PayController.kt
+++ src/main/kotlin/ru.mray.core/controller/PayController.kt
@@ -1,19 +1,29 @@
 package ru.mray.core.controller;
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import ru.mray.core.component.PricesHolder
+import ru.mray.core.exceptions.BadRequestException
 import ru.mray.core.model.Transaction
+import ru.mray.core.repository.TransactionRepository
 import ru.mray.core.service.W1Service
 import ru.mray.core.util.describe
-import java.text.DecimalFormat
+import java.net.URLEncoder
+import javax.servlet.http.HttpServletRequest
 
 
 @Controller
 @RequestMapping("/pay")
-class PayController(val w1Service: W1Service, val pricesHolder: PricesHolder) {
+class PayController(val w1Service: W1Service,
+                    val pricesHolder: PricesHolder,
+                    val transactionRepository: TransactionRepository) {
+
+    val logger = LoggerFactory.getLogger(PayController::class.java)
+
     @RequestMapping("/{transaction}")
     fun createForm(@PathVariable transaction: Transaction,
                    model: Model): String {
@@ -30,5 +40,32 @@ class PayController(val w1Service: W1Service, val pricesHolder: PricesHolder) {
         model.addAttribute("signature", signature)
 
         return "pay/form"
+    }
+
+    @ResponseBody
+    @RequestMapping("/confirm")
+    fun confirm(request: HttpServletRequest): String {
+        val signature = request.getParameter("WMI_SIGNATURE") ?: throw BadRequestException("No WMI_SIGNATURE field in request")
+        val params = request.parameterNames
+                .toList()
+                .filter { it != "WMI_SIGNATURE" }
+                .map { it to request.getParameter(it) }
+                .toMap()
+
+        val actualSignature = w1Service.sign(params)
+        if (signature != actualSignature) {
+            logger.warn("WMI_SIGNATURE: Actual: $actualSignature. Declared: $signature")
+            throw BadRequestException("WMI_SIGNATURE validation failed")
+        }
+
+        val transactionId = request.getParameter("WMI_PAYMENT_NO")
+                ?: throw BadRequestException("No WMI_PAYMENT_NO field in request")
+
+        transactionRepository.findOne(transactionId) ?: let {
+            val respStr = URLEncoder.encode("WMI_RESULT=RETRY&WMI_DESCRIPTION=Unknown transaction id", "UTF-8")
+            return respStr
+        }
+
+        return "WMI_RESULT=OK"
     }
 }
