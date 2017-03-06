11f792ed6d719e97e6b63d2335b71a05e6b143a6
Dmitry <dimarik-2@yandex.ru>
2017-03-06 17:44:10 +0300

    WIP #6

--- src/main/kotlin/ru.mray.core/util/periodUtils.kt
+++ src/main/kotlin/ru.mray.core/util/periodUtils.kt
@@ -2,7 +2,7 @@ package ru.mray.core.util
 
 import java.time.Period
 
-fun Period.describe(): String {
+fun Period. describe(): String {
     val periodTotalMonths = this.toTotalMonths()
     val periodDescription = when (periodTotalMonths.rem(10)) {
         1L -> "$periodTotalMonths месяц"
