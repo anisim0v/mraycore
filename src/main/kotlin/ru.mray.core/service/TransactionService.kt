04e89c83c540d37b1b80b3a470f5bcfc85e3b453
Dmitry Romanov <me@wutiarn.ru>
2017-03-07 22:41:39 +0300

    WIP #14

---
+++ src/main/kotlin/ru.mray.core/service/TransactionService.kt
@@ -0,0 +1,16 @@
+package ru.mray.core.service
+
+import org.springframework.stereotype.Service
+import ru.mray.core.model.Account
+import ru.mray.core.repository.TransactionRepository
+
+@Service
+class TransactionService(val transactionRepository: TransactionRepository) {
+    fun refreshAccountTransactions(account: Account) {
+        if (!account.provisioned) {
+            return
+        }
+
+        val inactivePaidTransactions = transactionRepository.findAccountInactivePaidTransactions(account.id)
+    }
+}
