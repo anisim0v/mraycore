bac4b7677236d397d148393275d8c0508472c3a0
Dmitry Romanov <me@wutiarn.ru>
2017-03-08 08:44:16 +0300

    Remove unfinished code (see #14) from master branch

--- src/main/kotlin/ru.mray.core/repository/TransactionRepository.kt
+++ src/main/kotlin/ru.mray.core/repository/TransactionRepository.kt
@@ -1,10 +1,6 @@
 package ru.mray.core.repository
 
 import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.data.mongodb.repository.Query
 import ru.mray.core.model.Transaction
 
-interface TransactionRepository : MongoRepository<Transaction, String> {
-    @Query("{ 'accountId': ?0, 'paidAt': { \$ne: null }, 'activatedAt': { \$exists: false } }")
-    fun findAccountInactivePaidTransactions(accountId: String): List<Transaction>
-}
+interface TransactionRepository : MongoRepository<Transaction, String>
