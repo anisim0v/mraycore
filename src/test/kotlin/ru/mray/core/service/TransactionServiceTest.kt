bec6da36d403f783ce96cb076d6fd5113b34cc7b
Dmitry Romanov <me@wutiarn.ru>
2017-03-08 19:57:50 +0300

    WIP #14 TransactionService tests

--- src/test/kotlin/ru/mray/core/service/TransactionServiceTest.kt
+++ src/test/kotlin/ru/mray/core/service/TransactionServiceTest.kt
@@ -1,46 +1,79 @@
 package ru.mray.core.service
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.mockito.Mockito
 import ru.mray.core.model.Account
 import ru.mray.core.model.Transaction
 import ru.mray.core.repository.TransactionRepository
 import java.time.Instant
+import java.time.OffsetDateTime
 import java.time.Period
-import java.util.*
+import java.time.ZoneId
+import java.time.temporal.ChronoUnit
 
 class TransactionServiceTest {
-    @Test
-    fun refreshAccountTransactions() {
-        val transactionRepository = Mockito.mock(TransactionRepository::class.java)
 
-        val accountId = UUID.randomUUID().toString()
+    val transactionRepository = Mockito.mock(TransactionRepository::class.java)
 
-        val activatedTransaction = Transaction(accountId, Account.Region.PH, Period.ofMonths(1), Transaction.TransactionType.PAYMENT)
-        activatedTransaction.activatedAt = Instant.now()
-        activatedTransaction.activeUntil = Instant.now().plusSeconds(30)
-        activatedTransaction.paidAt = Instant.now()
+    val account = Account("bob@example.com", Account.Region.PH, 1).let {
+        it.provisioned = true
+        return@let it
+    }
 
-        Mockito.`when`(transactionRepository.findLatestActiveAccountTransaction(accountId))
+    val activatedTransaction = Transaction(account.id, Account.Region.PH, Period.ofMonths(1), Transaction.TransactionType.PAYMENT).let {
+        it.activatedAt = Instant.now()
+        it.activeUntil = Instant.now().plus(10, ChronoUnit.DAYS)
+        it.paidAt = Instant.now()
+        return@let it
+    }
+
+    val paidTransaction = Transaction(account.id, Account.Region.PH, Period.ofMonths(1), Transaction.TransactionType.PAYMENT).let {
+        it.paidAt = Instant.now()
+        return@let it
+    }
+
+    val transactionService = TransactionService(transactionRepository)
+
+    init {
+        Mockito.`when`(transactionRepository.findLatestActiveAccountTransaction(account.id))
                 .thenReturn(activatedTransaction)
 
-        val paidTransaction = Transaction(accountId, Account.Region.PH, Period.ofMonths(1), Transaction.TransactionType.PAYMENT)
-        paidTransaction.paidAt = Instant.now()
-
-        Mockito.`when`(transactionRepository.findAccountInactivePaidTransactions(accountId))
+        Mockito.`when`(transactionRepository.findAccountInactivePaidTransactions(account.id))
                 .thenReturn(listOf(paidTransaction))
+    }
 
-        val transactionService = TransactionService(transactionRepository)
+    @Test
+    fun testRefreshAccountTransactions() {
+        transactionService.refreshAccountTransactions(account)
 
-        val account = Account("bob@example.com", Account.Region.PH, 1)
-        account.id = accountId
-        account.provisioned = true
+        assertThat(paidTransaction.previousTransactionId).isEqualTo(activatedTransaction.id)
+        assertThat(paidTransaction.activatedAt).isNotNull()
+        assertThat(paidTransaction.activeUntil)
+                .isEqualTo(OffsetDateTime.ofInstant(activatedTransaction.activeUntil!!, ZoneId.of("UTC"))
+                        .plus(paidTransaction.period)
+                        .toInstant())
+    }
+
+    @Test
+    fun testServiceUsesNow() {
+        activatedTransaction.activeUntil = Instant.now().minusSeconds(10)
 
         transactionService.refreshAccountTransactions(account)
 
-        Assertions.assertThat(paidTransaction.previousTransactionId).isEqualTo(activatedTransaction.id)
-        Assertions.assertThat(paidTransaction.activatedAt).isNotNull()
-        Assertions.assertThat(paidTransaction.activeUntil).isNotNull()
+        assertThat(paidTransaction.activeUntil).isGreaterThan(OffsetDateTime.ofInstant(activatedTransaction.activeUntil!!, ZoneId.of("UTC"))
+                .plus(paidTransaction.period)
+                .toInstant())
+    }
+
+    @Test
+    fun testNothingHappensWhenAccountIsNotProvisioned() {
+        account.provisioned = false
+
+        transactionService.refreshAccountTransactions(account)
+
+        assertThat(paidTransaction.previousTransactionId).isNull()
+        assertThat(paidTransaction.activatedAt).isNull()
+        assertThat(paidTransaction.activeUntil).isNull()
     }
 }
