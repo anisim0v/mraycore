a9489bf8725ff68c0c74334072f657b4598297b4
Dmitry Anisimov <dimarik-2@yandex.ru>
2017-03-23 21:14:19 +0300

    WIP #12
    Implement page with users in admin panel

--- src/main/kotlin/ru.mray.core/controller/admin/UsersPageController.kt
+++ src/main/kotlin/ru.mray.core/controller/admin/UsersPageController.kt
@@ -1,28 +1,26 @@
 package ru.mray.core.controller.admin
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import ru.mray.core.controller.PayController
-import ru.mray.core.model.Account
-import ru.mray.core.model.Transaction
 import ru.mray.core.repository.AccountRepository
-import ru.mray.core.repository.TransactionRepository
 
 
 @Controller
 @RequestMapping("admin/users")
 class UsersPageController(val accountRepository: AccountRepository) {
     val logger: Logger = LoggerFactory.getLogger(PayController::class.java)
+
     @RequestMapping
-    fun generateTable(model: Model): String {
-        val users = accountRepository.findAll()
-        model.addAllAttributes(users)
+    fun generateTable(@RequestParam(defaultValue="0") page: Int,
+                      model: Model): String {
+        val users = accountRepository.findAll(PageRequest(page, 50))
+        model.addAttribute("users", users)
 
         return "admin/users"
     }
