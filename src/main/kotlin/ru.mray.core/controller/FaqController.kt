914a06cf70172a9be3f79d2675cc7bce279bd32a
Dmitry Anisimov <dimarik-2@yandex.ru>
2017-03-23 22:50:11 +0300

    WIP #29
    Implement FAQ page

---
+++ src/main/kotlin/ru.mray.core/controller/FaqController.kt
@@ -0,0 +1,14 @@
+package ru.mray.core.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/faq")
+class FaqController{
+    @RequestMapping
+    fun faqController(): String {
+        return "faq/main"
+    }
+
+}
