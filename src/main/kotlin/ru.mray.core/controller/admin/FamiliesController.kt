f6b8b053781ccf20c5efb4e05f41e9673c211f0b
Dmitry Romanov <me@wutiarn.ru>
2017-03-24 18:55:47 +0300

    WIP #1
    Family add form

--- src/main/kotlin/ru.mray.core/controller/admin/FamiliesController.kt
+++ src/main/kotlin/ru.mray.core/controller/admin/FamiliesController.kt
@@ -3,7 +3,8 @@ package ru.mray.core.controller.admin
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
-import ru.mray.core.repository.AccountRepository
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import ru.mray.core.repository.FamilyRepository
 import ru.mray.core.repository.FamilyTokenRepository
 
@@ -18,5 +19,24 @@ class FamiliesController(val familyTokenRepository: FamilyTokenRepository,
         model.addAttribute("families", families)
 
         return "admin/familyList"
+    }
+
+    @RequestMapping("/add")
+    fun addFamilyPage(): String {
+        return "admin/familyAdd"
+    }
+
+    @RequestMapping("/add", method = arrayOf(RequestMethod.POST))
+    fun addFamily(
+            @RequestParam login: String,
+            @RequestParam password: String,
+            @RequestParam region: String,
+            @RequestParam city: String,
+            @RequestParam streetName: String,
+            @RequestParam streetNumber: String,
+            @RequestParam zipCode: String,
+            @RequestParam paidUntil: String,
+            @RequestParam tokens: List<String>): String {
+        return "admin/familyAdd"
     }
 }
