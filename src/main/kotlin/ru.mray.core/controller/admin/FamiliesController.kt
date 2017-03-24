26224b6f0b4f8b7644d88266f6589233a447efff
Dmitry Romanov <me@wutiarn.ru>
2017-03-24 19:06:07 +0300

    WIP #1
    Process family add form

--- src/main/kotlin/ru.mray.core/controller/admin/FamiliesController.kt
+++ src/main/kotlin/ru.mray.core/controller/admin/FamiliesController.kt
@@ -5,8 +5,12 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
+import ru.mray.core.model.Account
+import ru.mray.core.model.Family
+import ru.mray.core.model.FamilyToken
 import ru.mray.core.repository.FamilyRepository
 import ru.mray.core.repository.FamilyTokenRepository
+import java.time.LocalDate
 
 
 @Controller
@@ -30,13 +34,38 @@ class FamiliesController(val familyTokenRepository: FamilyTokenRepository,
     fun addFamily(
             @RequestParam login: String,
             @RequestParam password: String,
-            @RequestParam region: String,
+            @RequestParam region: Account.Region,
             @RequestParam city: String,
             @RequestParam streetName: String,
             @RequestParam streetNumber: String,
             @RequestParam zipCode: String,
             @RequestParam paidUntil: String,
             @RequestParam tokens: List<String>): String {
+
+        val paidUntilDate = LocalDate.parse(paidUntil)
+
+        val family = Family()
+        family.login = login
+        family.password = password
+        family.region = region
+        family.city = city
+        family.streetName = streetName
+        family.streetNumber = streetNumber
+        family.zipCode = zipCode
+        family.paidUntil = paidUntilDate
+        familyRepository.save(family)
+
+        tokens
+                .map {
+                    val familyToken = FamilyToken()
+                    familyToken.familyLogin = family.login
+                    familyToken.region = region
+                    familyToken.token = it
+                    return@map familyToken
+                }
+                .toList()
+                .let { familyTokenRepository.save(it) }
+
         return "admin/familyAdd"
     }
 }
