f98f1483c7621c60e22dd8ec3d58fb14234c9dfb
Dmitry Romanov <me@wutiarn.ru>
2017-04-13 14:05:16 +0300

    Code style changes

--- src/main/kotlin/ru.mray.core/controller/DonateController.kt
+++ src/main/kotlin/ru.mray.core/controller/DonateController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/donate")
 class DonateController {
     @RequestMapping
-    fun donatepage(): String {
+    fun donatePage(): String {
         return "donate"
     }
 }
--- src/main/kotlin/ru.mray.core/controller/FaqController.kt
+++ src/main/kotlin/ru.mray.core/controller/FaqController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/faq")
 class FaqController {
     @RequestMapping
-    fun faqpage(): String {
+    fun faqPage(): String {
         return "faq"
     }
 }
