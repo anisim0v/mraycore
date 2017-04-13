849c783b3289b3c077bebf9265bf5b735d49fe8f
Dmitry Romanov <me@wutiarn.ru>
2017-04-13 22:04:41 +0300

    /stats

---
+++ src/main/kotlin/ru.mray.core/controller/StatsController.kt
@@ -0,0 +1,35 @@
+package ru.mray.core.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+import ru.mray.core.controller.admin.AdminIndexController
+import ru.mray.core.model.Account
+import ru.mray.core.repository.AccountRepository
+import ru.mray.core.repository.FamilyTokenRepository
+import ru.mray.core.repository.TransactionRepository
+
+@Controller
+@RequestMapping("/stats")
+class StatsController(val accountRepository: AccountRepository,
+                      val transactionRepository: TransactionRepository,
+                      val familyTokenRepository: FamilyTokenRepository) {
+    @RequestMapping
+    fun statsPage(model: Model): String {
+        val activeCount = accountRepository.countByFamilyTokenIsNotNull()
+
+        val pendingAccounts = accountRepository.findPending()
+
+        val regionsStats = Account.Region.values().map { region ->
+            val pendingCount = pendingAccounts.filter { it.region == region }.count()
+            val unassignedTokensCount = familyTokenRepository.countUnassigned(region)
+            val tokenCountToAssign = arrayOf(pendingCount, unassignedTokensCount).min()!!
+
+            AdminIndexController.RegionStats(region, pendingCount, unassignedTokensCount, tokenCountToAssign)
+        }
+
+        model.addAttribute("activeCount", activeCount)
+        model.addAttribute("regionsStats", regionsStats)
+        return "stats"
+    }
+}
---
+++ src/main/resources/templates/stats.ftl
@@ -0,0 +1,21 @@
+<#include 'library/standardPage.ftl'>
+<#-- @ftlvariable name="regionsStats" type="ru.mray.core.controller.admin.AdminIndexController.RegionStats[]" -->
+<#-- @ftlvariable name="activeCount" type="java.lang.Number" -->
+
+<@standardPage title="FAQ">
+<main class="buy">
+    <div class="container">
+        <div class="row">
+            <div class="twelwe columns" id="stats">
+                <h3>Текущая статистика:</h3>
+                <div>Активных аккаунтов: ${activeCount}</div>
+                <#list regionsStats as regionStats>
+                    <p>
+                <div>Заявок в обработке [${regionStats.region}]: ${regionStats.pendingCount}</div>
+                <div>Свободных токенов [${regionStats.region}]: ${regionStats.unassignedTokenCount}</div>
+                </#list>
+            </div>
+        </div>
+    </div>
+</main>
+</@standardPage>
