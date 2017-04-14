2058de709224c14c0ce0f9c080c91c98e49ef7fb
Dmitry Romanov <me@wutiarn.ru>
2017-04-14 14:04:10 +0300

    Request logging

--- src/main/kotlin/ru.mray.core/MRayCoreApplication.kt
+++ src/main/kotlin/ru.mray.core/MRayCoreApplication.kt
@@ -1,7 +1,22 @@
 package ru.mray.core
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.filter.CommonsRequestLoggingFilter
+import javax.servlet.Filter
 
 
 @SpringBootApplication
-open class MRayCoreApplication
+@Configuration
+open class MRayCoreApplication {
+    @Bean
+    open fun logFilter(): Filter {
+        val filter = CommonsRequestLoggingFilter()
+        filter.setIncludeQueryString(true)
+        filter.setIncludeClientInfo(true)
+        filter.setIncludePayload(true)
+        filter.setMaxPayloadLength(5120)
+        return filter
+    }
+}
