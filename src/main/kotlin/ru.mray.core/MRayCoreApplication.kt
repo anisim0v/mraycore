4952ca1f24194e9d9e5a3ada781880ff54adbb1d
Dmitry Romanov <me@wutiarn.ru>
2017-05-16 19:30:05 +0300

    Set UTC as default timezone

--- src/main/kotlin/ru.mray.core/MRayCoreApplication.kt
+++ src/main/kotlin/ru.mray.core/MRayCoreApplication.kt
@@ -1,8 +1,12 @@
 package ru.mray.core
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.util.*
 
 
 @SpringBootApplication
 open class MRayCoreApplication {
+    init {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    }
 }
