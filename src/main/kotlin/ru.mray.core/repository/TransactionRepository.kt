c3515af6fb4cef7e871122ea2c288724a229ff54
Dmitry Romanov <me@wutiarn.ru>
2017-05-17 15:06:57 +0300

    WIP: TransactionRepository implementation

--- src/main/kotlin/ru.mray.core/repository/TransactionRepository.kt
+++ src/main/kotlin/ru.mray.core/repository/TransactionRepository.kt
@@ -1,44 +1,27 @@
 package ru.mray.core.repository
 
+import org.intellij.lang.annotations.Language
 import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import ru.mray.core.model.Account
 import ru.mray.core.model.Account.Region
 import ru.mray.core.model.Transaction
 
 @Repository
-interface TransactionRepository : JpaRepository<Transaction, String>, TransactionRepositoryCustom {
-
-}
-
-interface TransactionRepositoryCustom {
+interface TransactionRepository : JpaRepository<Transaction, String> {
     fun findByAccountId(accountId: String, sort: Sort? = Sort(Sort.Direction.DESC, "issueDate")): List<Transaction>
-    fun findFirstByAccountId(accountId: String, sort: Sort? = Sort(Sort.Direction.DESC, "issueDate")): Transaction?
+
+    @Language("PostgreSQL")
+    @Query("SELECT *\nFROM transactions\nWHERE paid_at IS NOT NULL AND active_since IS NULL AND region = :#{#region.toString}\nORDER BY paid_at", nativeQuery = true)
+    fun findInactivePaidTransactions(region: Region): List<Transaction>
+
+    @Language("PostgreSQL")
+    @Query("SELECT *\nFROM transactions\nWHERE paid_at IS NOT NULL AND active_since IS NULL AND account_id = #{#account.id}\nORDER BY paid_at", nativeQuery = true)
     fun findAccountInactivePaidTransactions(account: Account): List<Transaction>
-    fun findInactivePaidTransactions(region: Region, sort: Sort = Sort(Sort.Direction.ASC, "paidAt")): List<Transaction>
+
+    @Language("PostgreSQL")
+    @Query("SELECT *\nFROM transactions\nWHERE active_since IS NOT NULL AND account_id = #{#account.id}\nORDER BY issue_date DESC", nativeQuery = true)
     fun findLatestActiveAccountTransaction(account: Account): ru.mray.core.model.Transaction?
-}
-
-class TransactionRepositoryImpl : TransactionRepositoryCustom {
-    override fun findLatestActiveAccountTransaction(account: Account): Transaction? {
-        throw UnsupportedOperationException()
-    }
-
-    override fun findAccountInactivePaidTransactions(account: Account): List<Transaction> {
-        throw UnsupportedOperationException()
-    }
-
-    override fun findByAccountId(accountId: String, sort: Sort?): List<Transaction> {
-        throw UnsupportedOperationException()
-    }
-
-    override fun findFirstByAccountId(accountId: String, sort: Sort?): Transaction? {
-        throw UnsupportedOperationException()
-    }
-
-    override fun findInactivePaidTransactions(region: Region, sort: Sort): List<Transaction> {
-        throw UnsupportedOperationException()
-    }
-
 }
