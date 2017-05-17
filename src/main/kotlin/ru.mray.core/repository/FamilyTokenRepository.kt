1a272428478e76177379333fa348456af065a170
Dmitry Romanov <me@wutiarn.ru>
2017-05-17 14:54:54 +0300

    WIP: FamilyToken implementation

--- src/main/kotlin/ru.mray.core/repository/FamilyTokenRepository.kt
+++ src/main/kotlin/ru.mray.core/repository/FamilyTokenRepository.kt
@@ -1,7 +1,6 @@
 package ru.mray.core.repository
 
 import org.intellij.lang.annotations.Language
-import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -11,21 +10,14 @@ import ru.mray.core.model.FamilyToken
 import java.time.LocalDate
 
 @Repository
-interface FamilyTokenRepository : JpaRepository<FamilyToken, String>, FamilyTokenRepositoryCustom {
+interface FamilyTokenRepository : JpaRepository<FamilyToken, String> {
     @Language("PostgreSQL")
     @Query("SELECT count(*)\nFROM family_tokens\nWHERE region = :#{#region.toString()} AND account_id IS NULL AND assign_manually = FALSE AND exists(\n    SELECT *\n    FROM families\n    WHERE families.id = family_tokens.family_id AND families.paid_until > :#{#expiresAfter}\n)", nativeQuery = true)
     fun countUnassigned(@Param("region") region: Account.Region, @Param("expiresAfter") expiresAfter: LocalDate = LocalDate.now()): Int
-}
 
-interface FamilyTokenRepositoryCustom {
+    @Language("PostgreSQL")
+    @Query("SELECT *\nFROM family_tokens\nWHERE region = :#{#region.toString()} AND account_id IS NULL AND assign_manually = FALSE AND exists(\n    SELECT *\n    FROM families\n    WHERE families.id = family_tokens.family_id AND families.paid_until > :#{#expiresAfter}\n)\nORDER BY family_id, slot\nLIMIT 1", nativeQuery = true)
     fun findFirstUnassigned(region: Account.Region,
-                            expiresAfter: LocalDate = LocalDate.now(),
-                            sort: Sort = Sort(Sort.Order(Sort.Direction.ASC, "family"))): FamilyToken?
+                            expiresAfter: LocalDate = LocalDate.now()): FamilyToken?
 
-}
-
-class FamilyTokenRepositoryImpl : FamilyTokenRepositoryCustom {
-    override fun findFirstUnassigned(region: Account.Region, expiresAfter: LocalDate, sort: Sort): FamilyToken? {
-        throw UnsupportedOperationException()
-    }
 }
